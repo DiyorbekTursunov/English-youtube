5a1f7b6f9b6914ecfd5d940a4804197ee0063eda
DiyorbekTursunov <diyorbektursunov570@gmail.com>
2024-06-17 11:35:01 +0500

    first commit

--- src/components/ui_elements/video-card.tsx
+++ src/components/ui_elements/video-card.tsx
@@ -14,7 +14,7 @@ export default function VideoCard({ video }: VideoCardProps) {
             <div className="flex justify-between mt-[3px]">
                 <h2>{video.time}</h2>
                 <div className="flex items-center gap-1">
-                    <span>{video.reyting}</span>
+                    <span>{video.rating}</span>
                     <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <path d="M6.66667 10.18L10.7867 12.6667L9.69333 7.98L13.3333 4.82667L8.54 4.42L6.66667 0L4.79333 4.42L0 4.82667L3.64 7.98L2.54667 12.6667L6.66667 10.18Z" fill="#FFD233" />
                     </svg>
