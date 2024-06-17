faa94442a85a01a06d6f05accf29cedc928f304c
DiyorbekTursunov <diyorbektursunov570@gmail.com>
2024-06-17 14:35:30 +0500

    debug

--- src/components/ui_elements/menu_bar.tsx
+++ src/components/ui_elements/menu_bar.tsx
@@ -1,53 +1,53 @@
-import { useState } from "react"
-import menu_icon_1 from "../images/bottom_menu_bar/menu_1.svg"
-import menu_icon_2 from "../images/bottom_menu_bar/menu_2.svg"
-import menu_icon_3 from "../images/bottom_menu_bar/menu_3.svg"
-import menu_icon_4 from "../images/bottom_menu_bar/menu_4.svg"
+// import { useState } from "react"
+// import menu_icon_1 from "../images/bottom_menu_bar/menu_1.svg"
+// import menu_icon_2 from "../images/bottom_menu_bar/menu_2.svg"
+// import menu_icon_3 from "../images/bottom_menu_bar/menu_3.svg"
+// import menu_icon_4 from "../images/bottom_menu_bar/menu_4.svg"
 
-interface menuIsActiveType {
-    activeId: string;
-}
+// interface menuIsActiveType {
+//     activeId: string;
+// }
 
-interface menuBarDataType {
-    id: string;
-    icon: string;
-}
+// interface menuBarDataType {
+//     id: string;
+//     icon: string;
+// }
 
-export default function MenuBar() {
-    const [menuIsActive, setmenuIsActive] = useState<menuIsActiveType>({ activeId: "1" })
+// export default function MenuBar() {
+//     const [menuIsActive, setmenuIsActive] = useState<menuIsActiveType>({ activeId: "1" })
 
-    const [menuBarData] = useState<menuBarDataType[]>([
-        {
-            id: '1',
-            icon: menu_icon_1
-        },
-        {
-            id: '2',
-            icon: menu_icon_2
-        },
-        {
-            id: '3',
-            icon: menu_icon_3
-        },
-        {
-            id: '4',
-            icon: menu_icon_4
-        },
-    ])
+//     const [menuBarData] = useState<menuBarDataType[]>([
+//         {
+//             id: '1',
+//             icon: menu_icon_1
+//         },
+//         {
+//             id: '2',
+//             icon: menu_icon_2
+//         },
+//         {
+//             id: '3',
+//             icon: menu_icon_3
+//         },
+//         {
+//             id: '4',
+//             icon: menu_icon_4
+//         },
+//     ])
 
 
 
-    return (
-        <div className="bg-[#fff] w-full absolute bottom-0">
-            <div className="max-w-[500px] mx-auto px-3 py-[23px]">
-                <div className="flex justify-between ">
+//     return (
+//         <div className="bg-[#fff] w-full absolute bottom-0">
+//             <div className="max-w-[500px] mx-auto px-3 py-[23px]">
+//                 <div className="flex justify-between ">
                     
-                </div>
+//                 </div>
                 
-            </div>
-        </div>
-    )
-}
+//             </div>
+//         </div>
+//     )
+// }
 
 
-// https://83.69.139.250:55555/
+// // https://83.69.139.250:55555/
